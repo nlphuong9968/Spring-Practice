8f7f0a4dfbd8b60e45952406e65be98dc3e5fac9
Phuong Nguyen <pnguyen9968@gmail.com>
2024-05-09 08:59:33 +0700

    feat: [TICKET-03] spring-boot hibernate-jpa-crud

---
+++ 03-spring-boot-hibernate-jpa-crud/05-cruddemo-student-update/src/main/java/com/luv2code/cruddemo/dao/StudentDAOImpl.java
@@ -0,0 +1,73 @@
+package com.luv2code.cruddemo.dao;
+
+import com.luv2code.cruddemo.entity.Student;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.TypedQuery;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Repository
+public class StudentDAOImpl implements StudentDAO {
+
+    // define field for entity manager
+    private EntityManager entityManager;
+
+    // inject entity manager using constructor injection
+    @Autowired
+    public StudentDAOImpl(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
+    // implement save method
+    @Override
+    @Transactional
+    public void save(Student theStudent) {
+        entityManager.persist(theStudent);
+    }
+
+    @Override
+    public Student findById(int id) {
+        return entityManager.find(Student.class, id);
+    }
+
+    @Override
+    public List<Student> findAll() {
+        // create query
+        TypedQuery<Student> query = entityManager.createQuery("FROM Student", Student.class);
+
+        // return query results
+        return query.getResultList();
+    }
+
+    @Override
+    public List<Student> findByLastName(String lastName) {
+        // create query
+        TypedQuery<Student> query = entityManager.createQuery("FROM Student WHERE lastName=:theData", Student.class);
+
+        //set query parameters
+        query.setParameter("theData", lastName);
+
+        // return query results
+        return query.getResultList();
+    }
+
+    @Override
+    @Transactional
+    public void update(Student student) {
+        entityManager.merge(student);
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
